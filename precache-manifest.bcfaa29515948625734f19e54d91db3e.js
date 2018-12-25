5c9018b7fdd1d87099b695c605d586f283d6e9de
Thiraviaraj <thiraviaraj@ALIPL0931.local>
2018-12-25 20:09:36 +0530

    Updates

---
+++ precache-manifest.bcfaa29515948625734f19e54d91db3e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mydashie/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "845fcc9ae64ee7cc21f8",
+    "url": "/mydashie/static/css/main.5566a10d.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mydashie/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9ecc41160fd52b39de2f",
+    "url": "/mydashie/static/js/1.9ecc4116.chunk.js"
+  },
+  {
+    "revision": "15956061ace475f52efe",
+    "url": "/mydashie/static/js/runtime~main.15956061.js"
+  },
+  {
+    "revision": "845fcc9ae64ee7cc21f8",
+    "url": "/mydashie/static/js/main.845fcc9a.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mydashie/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mydashie/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mydashie/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "9ecc41160fd52b39de2f",
+    "url": "/mydashie/static/css/1.4e5e71f2.chunk.css"
+  },
+  {
+    "revision": "8af319470e9ff12db3d758e80a1f93d6",
+    "url": "/mydashie/index.html"
+  }
+];
