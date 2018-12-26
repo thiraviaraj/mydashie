4ff71ce6139823c3e22f3c3deca8f2858c8ac2af
thiraviaraj <thiraviaraj>
2018-12-26 13:26:38 +0530

    CORS issue resolved

--- src/Api/httpWrapper.js
+++ src/Api/httpWrapper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://5c230da35db0f6001345fea3.mockapi.io',
+    baseURL: '//5c230da35db0f6001345fea3.mockapi.io',
 });
 
 // headers: {
