ba495e2ff2587c24c952293f3759ffd362008418
thiraviaraj <thiraviaraj>
2018-12-26 13:28:37 +0530

    HTTPS issue resolve

--- src/Api/httpWrapper.js
+++ src/Api/httpWrapper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: '//5c230da35db0f6001345fea3.mockapi.io',
+    baseURL: 'https://5c230da35db0f6001345fea3.mockapi.io',
 });
 
 // headers: {
